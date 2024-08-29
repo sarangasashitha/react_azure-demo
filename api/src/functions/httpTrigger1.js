a5311adc1730dfa8dd43ff85c5f9e9c01148e66a
sarangasashitha <sashithasaranga@gmail.com>
2024-08-29 11:52:09 +0530

    add api

---
+++ api/src/functions/httpTrigger1.js
@@ -0,0 +1,14 @@
+const { app } = require("@azure/functions");
+
+app.http("httpTrigger1", {
+  methods: ["GET", "POST"],
+  authLevel: "anonymous",
+  handler: async (request, context) => {
+    context.log(`Http function processed request for url "${request.url}"`);
+
+    const name =
+      request.query.get("name") || (await request.text()) || "from the api";
+
+    return { body: `Hello, ${name}!` };
+  },
+});
--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [data, setData] = useState("");
+
+  useEffect(() => {
+    (async function () {
+      const { text } = await (await fetch(`/api/httpTrigger1`)).json();
+      setData(text);
+    })();
+  });
+
+  return <div>{data}</div>;
 }
 
 export default App;
